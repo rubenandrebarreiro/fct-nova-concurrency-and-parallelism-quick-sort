29c8414cb9b36afb15a5bd217e5dece8ec2b2b0e
Rúben André Barreiro <ruben.andre.letra.barreiro@gmail.com>
2020-03-31 20:21:09 +0100

    Some minor fixes added
    Some minor fixes added

--- quicksort_omp/src/qs-seq.c
+++ quicksort_omp/src/qs-seq.c
@@ -6,6 +6,8 @@ int partition(void *base, int low_index, int high_index, size_t size_type, int (
   // The pivot element of the current array/vector
   void *pivot = &high_index;
 
+  //printf("%d", *((int*) pivot));
+
   // The index of smaller element
   int i = (low_index - size_type);
 
@@ -32,7 +34,12 @@ int partition(void *base, int low_index, int high_index, size_t size_type, int (
 
 void quick_sort(void *base, int low_index, int high_index, size_t size_type, int (*compar)(const void *, const void *)) {
 
+  printf("%d\n", low_index);
+  printf("%d\n", high_index);
+
   if (low_index < high_index) {
+
+    printf("low_index < high_index");
 
     int partition_index = partition(base, low_index, high_index, size_type, compar);
 
@@ -54,14 +61,14 @@ void QSORT(void *base, size_t num_elems, size_t size_type, int (*compar)(const v
 
   for(int current_elem = 0; current_elem < num_elems; current_elem++) {
 
-    printf("%lf ", ((double *) &base)[current_elem]);
+    printf( "%lf ", *( (double *) base + current_elem ) );
 
   }
 
   printf("\n\n");
 
   // Your code goes here
-  quick_sort(base, 0, ( num_elems * (size_type - 1) ), size_type, compar);
+  quick_sort(base, 0, (num_elems-1), size_type, compar);
 
   printf("Final array/vector:\n");
 
